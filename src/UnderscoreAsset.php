006973f71d4e04f6681820a8e66be208b7d21d0c
MadAnd <dev@madand.net>
2015-04-15 22:27:19 +0300

    add actual code

---
+++ src/UnderscoreAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link https://github.com/MadAnd/yii2-underscore
+ * @copyright Copyright (c) 2015 MadAnd <dev@madand.net>
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace madand\underscore;
+
+use yii\web\AssetBundle;
+
+class UnderscoreAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/underscore';
+    public $js = [
+        'underscore-min.js',
+    ];
+}
